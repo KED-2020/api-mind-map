5364d3af45606ee314a0b0c2c17249487254d8b3
Derrick Bol <derrickbol33@gmail.com>
2020-10-24 21:15:43 +0800

    Add API explore to explore the api

---
+++ lib/resource_info.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: false
+
+require 'http'
+require 'yaml'
+
+####################################
+## Configuration
+####################################
+ACCESS_TOKEN = YAML.safe_load(File.read('config/secrets.yml'))['GITHUB_TOKEN']
+
+gh_response = {}
+gh_results = {}
+
+####################################
+## API
+####################################
+def format_api_path(path)
+  "https://api.github.com/#{path}"
+end
+
+def call_github(url, params = {})
+  HTTP
+    .headers(
+      'Accept' => 'application/vnd.github.mercy-preview+json',
+      'Authorization' => "token #{ACCESS_TOKEN}"
+    ).get(url, params: params)
+end
+
+def topics_to_query(topics)
+  return '' if topics.length.zero?
+
+  topics.reduce('') do |query, topic|
+    "#{query} topic:#{topic}"
+  end
+end
+
+####################################
+## HAPPY Resources request
+####################################
+
+# Search & Find Repository
+TOPICS = %w[tensorflow natural-language-processing].freeze
+SEARCH_QUERY = "pytorch-transformers in:readme #{topics_to_query(TOPICS)}".freeze
+
+search_repo_params = {
+  q: SEARCH_QUERY,
+  sort: 'starts',
+  order: 'desc'
+}
+
+repo_search_url = format_api_path('search/repositories')
+gh_response[repo_search_url] = call_github(repo_search_url, search_repo_params)
+
+repo_search = gh_response[repo_search_url].parse
+
+selected_resource = repo_search['items'][0]
+
+# Explore the selected repo
+gh_results['name'] = selected_resource['name']
+# transformers
+
+gh_results['homepage'] = selected_resource['homepage']
+# Should be https://huggingface.co/
+
+gh_results['url'] = selected_resource['html_url']
+# Should be https://github.com/huggingface/transformers
+
+gh_results['description'] = selected_resource['description']
+# Should be 🤗 Transformers: State-of-the-art Natural Language Processing for Pytorch and TensorFlow 2.0.
+
+gh_results['language'] = selected_resource['language']
+# Should be Python
+
+gh_results['topics'] = selected_resource['topics']
+
+####################################
+## BAD Resources request
+####################################
+
+# Bad Search Request
+
+LONG_SEARCH_QUERY = 10.times.map { ('a'..'z').to_a }.join
+invalid_query_url = format_api_path('search/repositories')
+
+gh_response[invalid_query_url] = call_github(invalid_query_url, { q: LONG_SEARCH_QUERY })
+# Should be The search is longer than 256 characters.
+
+# Invalid API endpoint
+invalid_endpoint_url = format_api_path('search/invalid')
+gh_response[invalid_endpoint_url] = call_github(invalid_endpoint_url, search_repo_params)
+
+####################################
+## Save Results
+####################################
+File.write('spec/fixtures/github_results.yml', gh_results.to_yaml)
