4748cb8c7300fa6e98426bbd3d53470579ebe050
eslywadan <esly.wadan@gmail.com>
2020-10-25 14:34:19 +0800

    revised the spec_helper on file path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,8 @@ require 'minitest/rg'
 require 'yaml'
 require_relative '../lib/github_api'
 
-CORRECT = YAML.safe_load(File.read("spec/fixtures/github_results.yml"))
-ACCESS_TOKEN = YAML.safe_load(File.read("config/secrets.yml"))['GITHUB_TOKEN']
+CORRECT = YAML.safe_load(File.read("#{__dir__}/fixtures/github_results.yml"))
+ACCESS_TOKEN = YAML.safe_load(File.read("#{__dir__}/../config/secrets.yml"))['GITHUB_TOKEN']
 
 TOPICS = %w[tensorflow natural-language-processing].freeze
 SEARCH_QUERY = 'pytorch-transformers in:readme'.freeze
