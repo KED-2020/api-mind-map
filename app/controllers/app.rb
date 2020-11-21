eae08015942147659227abe2020f10eea8c4ff87
Kyeg <k16272002@gmail.com>
2020-11-22 02:26:52 +0800

    Resolve ERR_TOO_MANY_DIRECT

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -21,36 +21,39 @@ module MindMap
         view 'home'
       end
 
+      # GET /404
       routing.on '404' do
-        routing.get do
-          view '404'
-        end
+        view '404'
       end
 
+      # GET /resource_nil
       routing.on 'resource_nil' do
         view 'resource_nil'
       end
 
       # Inbox
       routing.on 'inbox' do
+        
+        # GET /inbox/new
         routing.on 'new' do
           view 'new_inbox'
         end
 
+        # POST /inbox/
         routing.post do
           inbox_id = routing.params['inbox_id']
 
           # Redirect to the get request
-          routing.redirect "inbox/#{inbox_id}"
+          routing.redirect "/inbox/#{inbox_id}"
         end
 
         routing.on String do |inbox_id|
+          # GET /inbox/{inbox_id}
           routing.get do
             # Find the inbox specified by the url.
-            inbox = Repository::Inbox::For.klass(Entity::Inbox)
-                                          .find_url(inbox_id)
+            inbox = Repository::Inbox::For.klass(Entity::Inbox).find_url(inbox_id)
 
-            routing.redirect '404' unless inbox
+            routing.redirect '/404' unless inbox
 
             # Load the suggestions for an inbox.
             suggestions = Mapper::Inbox.new(App.config.GITHUB_TOKEN).suggestions
@@ -64,6 +67,7 @@ module MindMap
       # Resource
       routing.on 'resource' do
         routing.is do
+          # POST /resource
           routing.post do
             search_term = routing.params['search']
             tags_term = routing.params['tags']
@@ -76,20 +80,21 @@ module MindMap
             resource = Github::ResourceMapper
                        .new(MindMap::App.config.GITHUB_TOKEN)
                        .search(search_term, tags)
-            routing.redirect 'resource_nil' unless resource
+            routing.redirect '/resource_nil' unless resource
             # Add the repo to database
             saved_resource = Repository::For.entity(resource).find_or_create(resource)
 
             # Redirect viewer to resource details
-            routing.redirect "resource?resource_origin_id=#{saved_resource.origin_id}"
+            routing.redirect "/resource?resource_origin_id=#{saved_resource.origin_id}"
           end
 
+          # GET /resource
           routing.get do
             resource_origin_id = routing.params['resource_origin_id']
 
-            resource = Repository::For.klass(Entity::Resource)
-                                      .find_origin_id(resource_origin_id)
+            resource = Repository::For.klass(Entity::Resource).find_origin_id(resource_origin_id)
 
+            # GET /resource?resource_origin_id={resource_origin_id}
             view 'resource', locals: { resource: resource }
           end
         end
