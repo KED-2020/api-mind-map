af92c7784d6e560f4561282613657ac9b2813301
eslywadan <esly.wadan@gmail.com>
2020-11-22 22:07:11 +0800

    refactor View Step 1-Restructure Dir

--- app/presentation/init.rb
+++ app/presentation/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[models infrastructure controllers domain presentation]
+folders = %w[view_objects]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
