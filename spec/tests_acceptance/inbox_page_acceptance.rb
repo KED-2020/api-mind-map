f3ddb3f0cc52b3312db674c27bff8629634376a8
Kyeg <k16272002@gmail.com>
2020-11-29 00:30:08 +0800

    Add page object for inbox

--- spec/tests_acceptance/inbox_page_acceptance.rb
+++ spec/tests_acceptance/inbox_page_acceptance.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require_relative '../helpers/acceptance_helper'
+require_relative 'pages/inbox_page'
 require_relative 'pages/home_page'
 
-describe 'Homepage Acceptance Tests' do
+describe 'Project Page Acceptance Tests' do
   include PageObject::PageFactory
-  
+
   DatabaseHelper.setup_database_cleaner
 
   before do
@@ -17,71 +18,25 @@ describe 'Homepage Acceptance Tests' do
     @browser.close
   end
 
-  describe 'Visit Home page' do
+  describe 'Visit Inbox page' do
 
-    it '(Happy) should see some basic elements on homepage' do
-      # GIVEN: nothing
-      # WHEN: user goes to the homepage
+    it '(Happy) should see search content' do
+      # GIVEN: user adds an existent inbox URL and submit
+      good_inbox_id = '12345'
       visit HomePage do |page|
-        # THEN: user should see some basic elements & should not see a warning message 
-        _(page.all_elements_present?).must_equal true
-        # THEN: these basic elements should be correct (Not decoupled if we test the textfield!?)
-        _(page.all_elements_has_correct_text?).must_equal true
-      end
-    end
-
-    it '(HAPPY) should be able to request a Test Inbox (e.g. /inbox/12345)' do
-      # GIVEN: an 'inbox 12345' exists in the database
-
-      # WHEN: user goes to the homepage
-      visit HomePage do |page|
-        # WHEN: user adds an existent inbox URL and submit
-        good_inbox_id = '12345'
         page.request_inbox(good_inbox_id)
-        # THEN: user should be redirected to the 'inbox 12345'
-        _(page.in_inbox_page?).must_equal true
-        _(page.in_this_inbox_page?(good_inbox_id)).must_equal true
       end
-    end
 
-    it '(HAPPY) should be able to request a Guest Inbox (e.g. /inbox/guest-inbox)' do
-      # GIVEN: a 'guest inbox' exists in the database
+      
+      # WHEN: user goes to the inbox page
+      visit(InboxPage, using_params: { inbox_id: good_inbox_id }) do |page|
+        # THEN: they should see the searching results
+        _(page.all_elements_has_correct_text?).must_equal true
 
-      # WHEN: user goes to the homepage
-      visit HomePage do |page|
-        # WHEN: user clicks the guest inbox button
-        page.request_guest_inbox
-        # THEN: user should be redirected to the 'guest inbox'
-        _(page.in_inbox_page?).must_equal true
-        _(page.in_guest_inbox_page?).must_equal true
-      end
-    end
-
-    it '(HAPPY) should be able to request a New Inbox (e.g. /inbox/)' do
-      # GIVEN: a 'new inbox' exists in the database
-
-      # WHEN: user goes to the homepage
-      visit HomePage do |page|
-        # WHEN: user clicks the new inbox button
-        page.request_new_inbox
-        # THEN: user should be redirected to the 'new inbox'
-        _(page.in_inbox_page?).must_equal true
-      end
-    end
-
-    it '(SAD) should not be able to redirect to valid but non-existent inbox URL' do
-      # GIVEN: 'inbox 12345' is the only inbox exists in the database
-
-      # WHEN: user goes to the homepage
-      visit HomePage do |page|
-        # WHEN: user adds a nonexistent inbox URL (e.g. 54321) and submit
-        sad_inbox_id = '54321'
-        page.request_inbox(sad_inbox_id)
-        # THEN: user should not be redirected
-        _(page.in_this_inbox_page?(sad_inbox_id)).must_equal false
-        # THEN: user should see a warning message
-        _(page.warning_message_present?).must_equal true
-        _(page.is_warning_message?).must_equal true
+        suggestion_names = ['tensorflow', 'TensorFlow', 'TensorFlow-Examples']
+        _(page.suggestions[0].text).must_include suggestion_names[0]
+        _(page.suggestions[0].text).must_include suggestion_names[1]
+        _(page.suggestions[0].text).must_include suggestion_names[2]
       end
     end
 
