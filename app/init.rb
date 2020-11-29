6256569a0125ea073d6d15e08ab8d7856807be86
eslywadan <esly.wadan@gmail.com>
2020-11-29 15:08:00 +0800

    form-object branch for adding the form objects

--- app/init.rb
+++ app/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[models infrastructure controllers domain presentation]
+folders = %w[models infrastructure controllers domain forms presentation]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
