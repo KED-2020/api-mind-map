89273b071fbc00cb06d7c70a10af87dce114cbf6
Derrick Bol <derrickbol33@gmail.com>
2020-12-07 02:06:34 +0800

    Update controller routes to use HTTP::Response representers + remove unused routes

---
+++ spec/tests_acceptance/api_spec.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+require_relative '../helpers/spec_helper'
+require_relative '../helpers/vcr_helper'
+require_relative '../helpers/database_helper'
+require 'rack/test'
+
+def app
+  MindMap::App
+end
+
+describe 'Test API routes' do
+  include Rack::Test::Methods
+
+  VcrHelper.setup_vcr
+  DatabaseHelper.setup_database_cleaner
+
+  before do
+    VcrHelper.configure_vcr_for_github
+    DatabaseHelper.wipe_database
+  end
+
+  after do
+    VcrHelper.eject_vcr
+  end
+
+  describe 'Root route' do
+    it 'should successfully return root information' do
+      get '/'
+
+      _(last_response.status).must_equal 200
+
+      body = JSON.parse(last_response.body)
+      _(body['status']).must_equal 'ok'
+      _(body['message']).must_include 'api/v1'
+    end
+  end
+
+  describe 'Get inboxes route' do
+    it 'should return a the requested inbox' do
+      new_inbox = MindMap::Entity::Inbox.new(id: nil,
+                                             name: 'Test Inbox',
+                                             url: GOOD_INBOX_ID,
+                                             description: 'A test inbox',
+                                             suggestions: [])
+      saved_inbox = MindMap::Repository::Inbox::For.klass(MindMap::Entity::Inbox).create(new_inbox)
+
+      get "/api/v1/inboxes/#{saved_inbox.url}"
+
+      _(last_response.status).must_equal 200
+
+      inbox = JSON.parse last_response.body
+
+      _(inbox['name']).must_equal saved_inbox.name
+      _(inbox['description']).must_equal saved_inbox.description
+      _(inbox['url']).must_equal saved_inbox.url
+      _(inbox['suggestions'].count).must_equal 0
+    end
+
+    it 'should return an error for invalid inbox' do
+      get '/api/v1/inboxes/9999'
+
+      _(last_response.status).must_equal 400
+
+      response = JSON.parse(last_response.body)
+      _(response['message']).must_include 'not'
+    end
+  end
+
+  describe 'Add documents route' do
+    it 'should be able to add a document' do
+      post 'api/v1/favorites/documents', JSON.generate('html_url': PROJECT_URL)
+
+      _(last_response.status).must_equal 201
+
+      document = JSON.parse last_response.body
+      _(document['name']).must_equal CORRECT['document_name']
+      _(document['html_url']).must_equal CORRECT['document_html_url']
+      _(document['description']).must_equal CORRECT['document_description']
+
+      doc = MindMap::Representer::Document.new(
+        MindMap::Response::OpenStructWithLinks.new
+      ).from_json last_response.body
+
+      _(doc.links['self'].href).must_include 'http'
+    end
+
+    it 'should report error when the document is invalid' do
+      post 'api/v1/favorites/documents', JSON.generate('html_url': 'https://github.com/derrxb/invalid')
+
+      _(last_response.status).must_equal 404
+
+      response = JSON.parse(last_response.body)
+      _(response['message']).must_include 'not'
+    end
+  end
+
+  describe 'Get documents route' do
+    it 'should return a the requested documents' do
+      MindMap::Service::AddDocument.new.call(html_url: PROJECT_URL)
+      document_id = 1
+
+      get "/api/v1/favorites/documents/#{document_id}"
+
+      last_response.body
+
+      _(last_response.status).must_equal 200
+
+      document = JSON.parse last_response.body
+
+      _(document['name']).must_equal CORRECT['document_name']
+      _(document['html_url']).must_equal CORRECT['document_html_url']
+      _(document['description']).must_equal CORRECT['document_description']
+    end
+
+    it 'should return an error for invalid documents' do
+      get '/api/v1/favorites/documents/9999'
+
+      _(last_response.status).must_equal 400
+
+      response = JSON.parse(last_response.body)
+      _(response['message']).must_include 'not'
+    end
+  end
+end
