3c62cccd680788a1f7aa2d768edc2dd097f2a4cb
Kyeg <k16272002@gmail.com>
2020-12-06 18:02:15 +0800

    Add HATEOAS based on ROAR Hypermedia

--- app/presentation/representers/inbox_representer.rb
+++ app/presentation/representers/inbox_representer.rb
@@ -9,11 +9,23 @@ module MindMap
   module Representer
     class Inbox < Roar::Decorator
       include Roar::JSON
+      include Roar::Hypermedia
+      include Roar::Decorator::HypermediaConsumer
 
       property :name
       property :description
       property :url
       collection :suggestions, extend: Representer::Suggestion, class: OpenStruct
+
+      link :self do
+        "#{App.config.API_HOST}/api/v1/inboxs/#{inbox_name}"
+      end
+
+      private
+
+      def inbox_name
+        represented.name
+      end
     end
   end
 end
